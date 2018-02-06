44224e2e4250d1d77bef4d612ca382b4f1cd2fc8
tazbingor <852353298@qq.com>
2018-02-06 21:04:49 +0800

    Douban crawler

---
+++ get_movie.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import urllib
+import json
+import time
+import web
+
+db = web.database(dbn='sqlite', db='ShowFilm.db')
+
+
+def get_movie_ids():
+    movie_ids = []
+    for index in range(0, 250, 50):
+        # print index
+        response = urllib.urlopen('http://api.douban.com/v2/movie/top250?start=%d&count=50' % index)
+        data = response.read()
+        # print data
+
+        data_json = json.loads(data)
+        movie_tops = data_json['subjects']
+        for movie in movie_tops:
+            movie_ids.append(movie['id'])
+        time.sleep(3)
+    return movie_ids
+
+
+def add_movie(data):
+    movie = json.loads(data)
+    db.insert(
+        'movie',
+        id=int(movie['id']),
+        title=movie['title'],
+        origin=movie['original_title'],
+        url=movie['alt'],
+        rating=movie['rating']['average'],
+        image=movie['images']['large'],
+        directors=','.join([d['name'] for d in movie['directors']]),
+        casts=','.join([c['name'] for c in movie['casts']]),
+        year=movie['year'],
+        genres=','.join(movie['genres']),
+        countries=','.join(movie['countries']),
+        summary=movie['summary'],
+    )
+
+
+def main():
+    movie_ids = get_movie_ids()
+    count = 0
+    for movie_id in movie_ids:
+        response = urllib.urlopen('http://api.douban.com/v2/movie/subject/%s' % movie_id)
+        data = response.read()
+        add_movie(data)
+        count += 1
+        time.sleep(3)
+
+
+if __name__ == '__main__':
+    main()
--- tables.sql
+++ tables.sql
@@ -1,23 +1,30 @@
 CREATE TABLE movie (
   id,
   title,
+  origin,
+  url,
+  rating,
+  image,
+  directors,
+  casts,
   year,
-  country,
-  abstract
+  genres,
+  countries,
+  summary
 );
 
-INSERT INTO movie VALUES (
-  1,
-  'Once Upon a Time in the West',
-  1968,
-  'USA',
-  'C era una volta il West'
-);
-
-INSERT INTO movie VALUES (
-  2,
-  'The Good, the Bad and the Ugly',
-  1966,
-  'USA',
-  'Il buono, il brutto, il cattivo.'
-);
+-- INSERT INTO movie VALUES (
+--   1,
+--   'Once Upon a Time in the West',
+--   1968,
+--   'USA',
+--   'C era una volta il West'
+-- );
+--
+-- INSERT INTO movie VALUES (
+--   2,
+--   'The Good, the Bad and the Ugly',
+--   1966,
+--   'USA',
+--   'Il buono, il brutto, il cattivo.'
+-- );
